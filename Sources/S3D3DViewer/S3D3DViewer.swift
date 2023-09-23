7b830d882205d3aca7fde7b10e4e87988d1364d1
hassan <hassanvfx@gmail.com>
2023-09-22 22:59:50 -0600

    initial commit

---
+++ Sources/S3D3DViewer/S3D3DViewer.swift
@@ -0,0 +1,10 @@
+public struct S3D3DViewer {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func test(){
+        print("S3D3DViewerLib linked!")
+    }
+}
